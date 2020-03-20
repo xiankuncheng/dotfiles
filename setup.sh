92e145e0545671c6fb1c3bbeed70d92fd06f8c26
Xiankun Cheng <xiankuncheng@gmail.com>
2020-03-20 18:19:01 +1300

    Added setup script.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#neovim config
+cp -Rv .config ~/
+#Alfred
+cp -Rv Alfred/Alfred.alfredpreferences ~/Library/Application\ Support/Alfred/
+#Hammerspoon
+cp -Rv .hammerspoon ~/
+#iTerm
+cp -Rv iTerm/com.googlecode.iterm2.plist ~/Library/Preferences
+#zshrc
+cp .zshrc ~/
