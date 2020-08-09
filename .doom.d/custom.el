d54813be5b6684f2e196591ff44c2fab289be0a2
Xiankun Cheng <xiankuncheng@gmail.com>
2020-08-10 06:30:28 +1200

    chore: emacs emoji

---
+++ .doom.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/xiankuncheng/org/careerladder.org" "/Users/xiankuncheng/org/projects.org" "/Users/xiankuncheng/org/scripts.org" "/Users/xiankuncheng/org/todo.org"))
+ '(package-selected-packages
+   '(cnfonts org-superstar jenkins winum emojify flow-js2-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#21242b")))))
