88eb155500617ea17908efa8e7ef8a7df43993b6
Xiankun Cheng <xiankuncheng@gmail.com>
2020-09-03 06:27:39 +1200

    chore: remove comment out settings

--- sync_settings_to_git.sh
+++ sync_settings_to_git.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-#cp -Rv ~/.zshrc ./
-#cp -Rv ~/.config/nvim ./.config
 cp -Rv ~/Library/Application\ Support/Alfred/Alfred.alfredpreferrences ./Alfred
-#cp -Rv ~/.hammerspoon ./
 cp -Rv ~/Library/Preferences/com.googlecode.iterm2.plist ./iTerm
 cp -Rv ~/.spacemacs ./
 cp -Rv ~/.emacs-profile ./
